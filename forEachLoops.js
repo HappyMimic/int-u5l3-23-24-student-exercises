72d5c97015b1c350cbacb9962c6a8532f93c1ce9
M. Jones <miriam.j.jones.work@gmail.com>
2026-06-24 13:37:07 -0500

    6.24.26 1.37pm

--- forEachLoops.js
+++ forEachLoops.js
@@ -19,16 +19,26 @@ colors.forEach(function(color){
 let numbers = [1, 2, 3, 4, 5];
 let sum = 0;
 // 3. Use a forEach loop to calculate and print the sum of all the elements in the array.
+numbers.forEach(function(number) {
+    sum += number;
+});
+console.log(sum);
 //  - There's a couple ways to solve this one - think through the logic and ask for support if you need it :)
 
 
 
 let fruits = ["apple", "banana", "cherry"]
 // 4. Use a forEach loop to print each element to the console.
+fruits.forEach(function(fruit){
+    console.log(fruit);
+});
 
 
 
 let names = ["Simba", "Scar", "Nala", "Mufasa", "Zazu"];
 // 5. Use a forEach loop to print each name along with its index number.
+names.forEach(function(name){
+    console.log(`Index #${names.indexOf(name)}: ${name}`)
+});
 //  - Remember: the index can be put as a second parameter.
 //  - Example: the first message should read something like "Simba (Index: 0)"
